c029f53107353619504ab85edb76de1b31ec7dfe
Vlad <hernandez87v@hotmail.com>
2020-03-12 01:28:56 +0000

    Adding wasd function for key presses

--- input.js
+++ input.js
@@ -2,16 +2,37 @@
  * Setup User Interface
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
+// Stores the active TCP connection object.
+let connection;
+
+handleUserinput = key => {
+  // \u0003 maps to ctrl+c input
+  if (key === '\u0003') {
+    process.exit();
+  }
+  if (key === 'w') {
+    connection.write('Move: up');
+  }
+  if (key === 'a') {
+    connection.write('Move: left');
+  }
+  if (key === 's') {
+    connection.write('Move: down');
+  }
+  if (key === 'd') {
+    connection.write('Move: right');
+  }
+};
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
+
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  stdin.on('data', handleUserInput => {
-    if (handleUserInput === '\u0003') {
-      process.stdout.write('.\x07');
-    }
-  });
+  stdin.on('data', handleUserinput);
+
   return stdin;
 };
 
